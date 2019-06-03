d5fd8846e1a12cb5883ddc8234474b9e20916641
wooline <wooline@qq.com>
2019-06-03 22:29:31 +0800

    基础框架

--- packages/core/dist/lib/index.js
+++
@@ -1,7 +0,0 @@
-export { ActionTypes, errorAction } from './actions';
-export { reducer, effect, logger, isServer, getClientStore, delayPromise } from './basic';
-export { viewWillMount, viewWillUnmount } from './store';
-export { setLoading, setLoadingDepthTime } from './loading';
-export { LoadingState } from './sprite';
-export { renderApp, renderSSR, BaseModuleHandlers, exportModule, exportModel, loadModel, isPromiseModule, isPromiseView, getView } from './module';
-//# sourceMappingURL=index.js.map
