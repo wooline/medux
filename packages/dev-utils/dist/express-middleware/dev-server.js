644b592224352f8db20c5b0ee43326f2021fae46
wooline <wooline@qq.com>
2020-05-07 18:30:18 +0800

    add errorcode 303 for ssr

--- packages/dev-utils/dist/express-middleware/dev-server.js
+++ packages/dev-utils/dist/express-middleware/dev-server.js
@@ -56,6 +56,15 @@ module.exports = function middleware(enableSSR, proxyMap, replaceTpl) {
                             res.redirect(parseInt(err.code, 10), err.detail);
                         }
                     }
+                    else if (err.code === '303') {
+                        if (res.headersSent) {
+                            res.write(htmlChunks[1].replace(/[^>]*<!--\s*{html}\s*-->[^<]*/m, ``).replace(/<!--\s*{script}\s*-->/, ``));
+                            res.end();
+                        }
+                        else {
+                            res.send(htmlChunks[0].replace(/[^>]*<!--\s*{html}\s*-->[^<]*/m, ``).replace(/<!--\s*{script}\s*-->/, ``));
+                        }
+                    }
                     else {
                         console.error(err);
                         if (res.headersSent) {
