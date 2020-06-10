99fc66ce35700f64fdd62bc45d7fa10b6e575480
wooline <wooline@qq.com>
2020-06-11 01:06:51 +0800

    拦截微信小程序物理按键返回

--- packages/react-web-router/dist/umd/index.js
+++ packages/react-web-router/dist/umd/index.js
@@ -4275,10 +4275,12 @@
         search && (searchStrings[index] = search);
         hash && (hashStrings[index] = hash);
       });
+      var search = joinSearchString(searchStrings).substr(1);
+      var hash = joinSearchString(hashStrings).substr(1);
       return {
         pathname: pathname,
-        search: '?' + joinSearchString(searchStrings).substr(1),
-        hash: '#' + joinSearchString(hashStrings).substr(1)
+        search: search ? '?' + search : '',
+        hash: hash ? '#' + hash : ''
       };
     };
 
